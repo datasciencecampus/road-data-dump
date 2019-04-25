2162ffc6e225fb2ebef5422d5e1f1ec131985a03
phil8192 <phil@parasec.net>
2019-04-25 15:02:35 +0000

    date range args + fix tail -n+2 bug

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # road-data-dump
 [![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](http://www.repostatus.org/badges/latest/wip.svg)](http://www.repostatus.org/#wip)
 
@@ -6,3 +7,8 @@
 Current dump (Nov'18 - Feb'19): http://parasec.net/road-data/
 
 Idea if this repo is to use [datasciencecampus/webtri.sh](https://github.com/datasciencecampus/webtri.sh) to snatch data then push to some remote location.
+
+
+## Prerequisites
+
+Install [JQ](https://stedolan.github.io/jq/) - the JSON command line processor.
--- dump.sh
+++ dump.sh
@@ -1,14 +1,22 @@
-# dockerise this..
-git clone https://github.com/phil8192/webtri.sh.git
+# todo dockerise this..
+
+# from,to range. DD-MM-YYYY 
+FROM=01032019
+TO=31032019
+
+git clone https://github.com/datasciencecampus/webtri.sh.git
 cd webtri.sh
 ./webtri.sh -f get_site_by_type -a 1 >  ../sites.csv
 ./webtri.sh -f get_site_by_type -a 2 >> ../sites.csv
 ./webtri.sh -f get_site_by_type -a 3 >> ../sites.csv
 mkdir ../data/
 
-time for i in $(tail +2 ../sites.csv |awk -F ',' '{print $1}' |tr -d '"') ;do
+#test first 10
+#head -10 ../sites.csv >x ;mv -v x ../sites.csv
+
+time for i in $(tail -n+2 ../sites.csv |awk -F ',' '{print $1}' |tr -d '"') ;do
   echo "get site id ${i}..."
-  ./webtri.sh -f get_report -a "$i daily 01112018 31032019" > ../data/"$i".csv
+  ./webtri.sh -f get_report -a "$i daily $FROM $TO" > ../data/"$i".csv
 done
 
 # combo..
