12c443916aee22db9607664c3ca5da10930371bd
r-leyshon <richard.leyshon@ons.gov.uk>
2020-12-16 09:15:57 +0000

    Output filenames appended with daterange of query

--- munge/15-write.R
+++ munge/15-write.R
@@ -2,10 +2,34 @@
 Write out product of analysis to csv to /output_data/ folder
 "
 info(my_logger, paste0("#############", "Start of", this.path(), "#############"))
+
+# create filenames --------------------------------------------------------
+
+
+# get the numbers from the daterange whether a test run or not
+dates_used <- paste(unlist(str_extract_all(daterange, "[0-9]+")), collapse = "to")
+
+
+suffix <- paste0(dates_used, ".csv")
+
+# write to file -----------------------------------------------------------
+# if testing then prepend files with "test-run"
+
+if (test_run == TRUE){
+  # write out csvs
+  fwrite(midas, paste0("output_data/test-midas", suffix), row.names=F, quote=F)
+  fwrite(tame, paste0("output_data/test-tame", suffix), row.names=F, quote=F)
+  fwrite(tmu, paste0("output_data/test-tmu", suffix), row.names=F, quote=F)
+} else {
+# if not testing, append filename with daterange
 # write out csvs
-fwrite(midas, "output_data/midas.csv", row.names=F, quote=F)
-fwrite(tame, "output_data/tame.csv", row.names=F, quote=F)
-fwrite(tmu, "output_data/tmu.csv", row.names=F, quote=F)
+fwrite(midas, paste0("output_data/midas", suffix), row.names=F, quote=F)
+fwrite(tame, paste0("output_data/tame", suffix), row.names=F, quote=F)
+fwrite(tmu, paste0("output_data/tmu", suffix), row.names=F, quote=F)
+
+}
+
+# wrap up -----------------------------------------------------------------
 
 # calculate elapsed time
 elapsed <- Sys.time() - start_time
