3c7664678400b925f7073739fa1b4911c8d5a0e7
r-leyshon <richard.leyshon@ons.gov.uk>
2020-12-29 09:23:42 +0000

    outputting validated Email to cache

--- app/server.R
+++ app/server.R
@@ -2,16 +2,22 @@ source("dependencies.R")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    
-    output$Email_check <- renderText({
-        validate(
-            need(expr = isValidEmail(input$userEmail),
-                 message = paste("Please Input a valid E-mail address"))
 
-        )
-        input$userEmail
-    
-    })
+# Email_check -------------------------------------------------------------
+# produce Email text
+    output$Email_check <- renderText({
+        # check format
+        validate(
+            # use basic Email format check in dependencies.R
+            need(expr = isValidEmail(input$userEmail),
+                 # if invalid, output a warning to user
+                 message = paste(input$userEmail, "is invalid. Please Input a valid E-mail address"))
+        ) #end of validate
+        # if validated, save the text string for pipeline execution
+        saveRDS(object = input$userEmail, file = "../cache/user_Email.rds")
+        # return the validation message
+        paste(input$userEmail, "is valid")
+    }) # end of Email_check
 
 
 })
