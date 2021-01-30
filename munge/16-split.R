0464abfc54a2202572cf44d301a8c708a1ee36dc
r-leyshon <richard.leyshon@ons.gov.uk>
2021-01-30 06:59:19 +0000

    updating 1.4 for column name changes requested by user

--- munge/16-split.R
+++ munge/16-split.R
@@ -3,15 +3,19 @@ Split files processed in prep.R by site
 "
 log4r::info(my_logger, paste0("############# ", "Start of ", current_file(), " #############"))
 
+
+# DAP pipeline expects column speed_avg_mph instead of Avg mph. Rename.
+combo <- rename(combo, speed_avg_mph = `Avg mph`)
+
 # split into midas,tame,tmu
 if(pipeline_message != "Queried dates are empty.") {
   midas <- combo[combo$type == "midas", ]
   tame <- combo[combo$type == "tame", ]
   tmu <- combo[combo$type == "tmu", ]
-
-
-# tidy up -----------------------------------------------------------------
-
+  
+  
+  # tidy up -----------------------------------------------------------------
+  
   rm(combo)
 }
 
