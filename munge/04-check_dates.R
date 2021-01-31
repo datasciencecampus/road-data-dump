4344891c65a5b78da6af676be166efce11967646
r-leyshon <richard.leyshon@ons.gov.uk>
2021-01-31 19:57:11 +0000

    removing defunct date handling logic from pipeline

--- munge/04-check_dates.R
+++ munge/04-check_dates.R
@@ -1,8 +1,5 @@
 "Purpose of script:
-Check input dates and break early if:
-* more than 31 days
-* end date is earlier than start date
-* not a viable date.
+Create daterange query parameter from shiny UI output parameters
 "
 log4r::info(my_logger, paste0("############# ", "Start of ", current_file(), " #############"))
 
@@ -16,27 +13,8 @@ if (test_run == FALSE) {
   s_date <- as.Date.character(start_date, "%d%m%Y")
   e_date <- as.Date.character(end_date, "%d%m%Y")
   
-  # incorrect dates such as 32092020 result in NAs that propogate forwards
-  # catch these here and break
-  # if(is.na(s_date) | is.na(e_date)){
-  #   error(my_logger, "Execution halted, NAs found in start or end date. Please
-  #       adjust start_date and end_date in munge/03-set_query_parameters.R")
-  #   stop("Invalid date set.")
-  # }
-  
   # find out the date period
   date_diff <- e_date - s_date
-  
-  # halt execution if not testing and date range requested is larger than one calendar month
-  # if(date_diff > 31) {
-  #   error(my_logger, "Execution halted, date period requested exceeds 31 days. Please
-  #       adjust start_date and end_date in munge/03-set_query_parameters.R")
-  #   stop("Date limit exceeded 31 days")
-  # } else if(date_diff < 0) {
-  #   error(my_logger, "Execution halted, end date is before start date. Please
-  #       adjust start_date and end_date in munge/03-set_query_parameters.R")
-  #   stop("End date is before start date.")
-  # }
   
   # date_range --------------------------------------------------------------
   
